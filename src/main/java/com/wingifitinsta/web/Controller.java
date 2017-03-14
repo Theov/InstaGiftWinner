935726729e0f42d28f0c1edb590aef6d62a1a23c
Maïlys A <ma.airouche@groupeonepoint.com>
2017-03-14 22:41:39 +0100

    Authentication : acces au token

--- src/main/java/com/wingifitinsta/web/Controller.java
+++ src/main/java/com/wingifitinsta/web/Controller.java
@@ -1,18 +1,35 @@
 package com.wingifitinsta.web;
 
-import com.wingifitinsta.service.ScheduledService;
+import java.lang.annotation.Annotation;
+
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.wingifitinsta.service.InstagramService;
 
 @RestController
 public class Controller {
+	
     @Autowired
-    private ScheduledService scheduledService;
+    private InstagramService authentication;
 
-    @RequestMapping("/")
-    public String entryPoint(){
-        scheduledService.hello();
-        return "Hello";
+    
+    @RequestMapping("/test")
+    public String redirectAuthorize(@RequestParam String code){
+    	if(code.contains("access_token")){
+    		System.out.println(code);
+    	}else{
+    		System.out.println(code);
+    		authentication.getAuthorize(code);
+    	}
+    	return "";
     }
+    
+    
 }
