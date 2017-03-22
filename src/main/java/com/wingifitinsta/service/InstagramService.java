cd7bcd5bb93c80cbe21779c78a1fdb9d87f073a6
Maïlys A <ma.airouche@groupeonepoint.com>
2017-03-22 23:39:16 +0100

    ajout liste amis

--- src/main/java/com/wingifitinsta/service/InstagramService.java
+++ src/main/java/com/wingifitinsta/service/InstagramService.java
@@ -1,12 +1,77 @@
 package com.wingifitinsta.service;
 
-import org.springframework.scheduling.annotation.Scheduled;
+import java.util.ArrayList;
+import java.util.List;
 
-@org.springframework.stereotype.Service
-public class ScheduledService {
+import org.apache.log4j.Logger;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
-    @Scheduled(cron="*/5 * * * *")
-    public void hello(){
-        System.out.println("Hello ;)");
-    }
+import com.wingiftinsta.model.FriendJson;
+import com.wingiftinsta.model.FriendsJson;
+import com.wingiftinsta.model.TokenJson;
+
+@Service
+public class InstagramService {
+	
+	private static final String HOST = "https://api.instagram.com";
+	private static final String ENDPOINT_ACCESS_TOKEN = "/oauth/access_token";
+	private static final String ENDPOINT_FOLLOWED_BY = "/v1/users/self/follows";
+
+	static Logger log = Logger.getLogger(InstagramService.class);
+			
+	private RestTemplate rest;
+	
+	private String accessToken;
+		
+	
+	public String getTokenBy(String code){
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		
+		rest = new RestTemplate();
+		MultiValueMap<String, String> parameters = new LinkedMultiValueMap<String, String>();
+		parameters.add("client_id", "");
+		parameters.add("client_secret","");
+		parameters.add("grant_type","authorization_code");
+		parameters.add("redirect_uri","http://localhost:8083/test");
+		parameters.add("code",code);
+		
+		TokenJson response = null;
+		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(parameters, headers);
+		try{
+			response = rest.postForObject(HOST + ENDPOINT_ACCESS_TOKEN, request, TokenJson.class);
+			log.info(response);
+		}catch(HttpClientErrorException e){
+			System.out.println(e.getResponseBodyAsString());
+		}
+		this.accessToken = response.getToken();
+		return accessToken;
+	}
+		
+	
+	public ArrayList<FriendJson> getRelationships(){
+		rest = new RestTemplate();
+		FriendsJson subscriptionList = null;
+		try{
+			subscriptionList = rest.getForObject(HOST + ENDPOINT_FOLLOWED_BY + "?access_token=" + this.getToken(), FriendsJson.class);
+			log.info(subscriptionList.getData());
+		}catch(HttpClientErrorException e){
+			e.printStackTrace();
+		}
+		return subscriptionList.getData();
+	}
+
+
+	private String getToken() {
+		return this.accessToken;
+	}
+	
+	
 }
--- src/main/java/com/wingifitinsta/web/Controller.java
+++ src/main/java/com/wingifitinsta/web/Controller.java
@@ -1,35 +1,31 @@
 package com.wingifitinsta.web;
 
-import java.lang.annotation.Annotation;
-
-import javax.websocket.server.PathParam;
+import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wingifitinsta.service.InstagramService;
+import com.wingiftinsta.model.FriendJson;
 
 @RestController
 public class Controller {
 	
     @Autowired
-    private InstagramService authentication;
+    private InstagramService service;
 
     
     @RequestMapping("/test")
     public String redirectAuthorize(@RequestParam String code){
-    	if(code.contains("access_token")){
-    		System.out.println(code);
-    	}else{
-    		System.out.println(code);
-    		authentication.getAuthorize(code);
-    	}
-    	return "";
+    	return service.getTokenBy(code);
     }
     
+    
+    @RequestMapping("/friends")
+    public ArrayList<FriendJson> getFriends(){
+    	return service.getRelationships();
+    }
     
 }
